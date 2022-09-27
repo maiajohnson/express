47c7c3e76ccd2a05cccd4be03f92cc68b2f86e35
Maia Johnson <maiaj1306@gmail.com>
2022-09-27 16:35:24 -0500

    more lecture notes; added res, .send, and .get functions

--- Server/server.js
+++ Server/server.js
@@ -2,9 +2,28 @@ const express = require('express');
 
 const app = express();
 
+let comments = [
+    'servers yayyy',
+    'actually tho',
+    'helooooo?'
+]
+
 // Make the server/public folder
 // available to who wants them
 app.use(express.static('./server/public'));
+
+app.get('/comments', (req, res) => {
+    console.log("Gimme some comments");
+
+    res.send(comments);
+
+});
+
+app.get('/comments/first', (req, res) => {
+    console.log('in /comments/first');
+
+    res.send(comments[0]);
+});
 
 app.listen(2000, () => {
     console.log('We\'re alive!');
